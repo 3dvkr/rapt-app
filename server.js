1b9c6a7653060b1a58fd748840c108bc23ceb6e0
dvkr <v3dvkr@gmail.com>
2021-05-13 08:43:46 -0600

    established connection with MongoDB, created User model

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
+const connectDB = require("./config/database");
+
+require("dotenv").config({ path: "./config/.env" });
+connectDB()
 // app.set("view engine", "ejs");
 
 const mainRoutes = require('./routes/main')
