fa7b04d249f8b8c215d34c0753fd068a1bc3d480
dvkr <v3dvkr@gmail.com>
2021-05-14 12:38:02 -0600

    added basic auth
    also created model for User, new routes and controllers for auth
    
    minor restructuring in views folder

--- routes/main.js
+++ routes/main.js
@@ -1,8 +1,17 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const homeController = require('../controllers/home')
+const homeController = require("../controllers/home");
+const authController = require("../controllers/auth");
 
-router.get('/', homeController.hello)
+router.get("/", homeController.hello);
 
-module.exports = router
+router.get("/login", authController.getLogin);
+router.post("/login", authController.postLogin);
+
+router.get("/logout", authController.logout);
+
+router.get("/signup", authController.getSignup);
+router.post("/signup", authController.postSignup);
+
+module.exports = router;
