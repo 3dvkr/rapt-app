f1cb66981c01ece9e0b62e067309c9fb42b5b1a8
dvkr <v3dvkr@gmail.com>
2021-05-17 05:57:15 -0600

    added ejs for app's timer

--- controllers/timers.js
+++ controllers/timers.js
@@ -1,8 +1,10 @@
 const Timer = require("../models/Timer");
 
 module.exports = {
-    workSession: (req, res) => {
-        console.log('main timer route works')
-        res.send("timer route")
-    }
-}
+  workSession: (req, res) => {
+    console.log("main timer route works");
+    // res.send("timer route")
+
+    res.render("workSession");
+  },
+};
