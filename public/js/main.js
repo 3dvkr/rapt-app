cdacf07f0f34ed7e303ef1c718b0d7d035ea6c06
dvkr <v3dvkr@gmail.com>
2021-05-28 07:13:36 -0600

    added feature: default/suggested times for work and break

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,26 @@
+// Default time for session types:
+let inputs = document.querySelectorAll('input[name="sessionType"]');
+let sessionType;
+inputs.forEach((el) =>
+  el.addEventListener("click", () => {
+    Array.from(inputs).forEach((el) => {
+      el.parentNode.children[1].classList.remove("selected");
+    });
+
+    Array.from(inputs)
+      .filter((el) => el.checked)
+      .forEach((el) => {
+        el.parentNode.children[1].classList.add("selected");
+        let duration = document.querySelector('input[name="duration"]');
+        duration.focus();
+        sessionType = el.value;
+        duration.value = el.value == "work" ? 2 : 1; // fix
+      });
+  })
+);
+
+
+// Count down feature
 let timeString;
 
 const startBtn = document.getElementById("startBtn");
