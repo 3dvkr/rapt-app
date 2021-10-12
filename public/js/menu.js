ac8300e6f7bc08d595592427bcdb0120d4c3e73b
dvkr <v3dvkr@gmail.com>
2021-10-12 17:31:28 -0600

    refactored and restyled nav menu

--- public/js/menu.js
+++ public/js/menu.js
@@ -2,5 +2,4 @@ const navAccess = document.getElementById('navAccess');
 console.log(navAccess);
 navAccess.addEventListener('click', () => {
   document.getElementById('mainNav').classList.toggle('hideNav');
-  navAccess.textContent == '>' ? navAccess.textContent = '<' : navAccess.textContent = '>';
 });
